833cd1b5aa118702ed3987959c2816ad77f572c8
Matis <matis.baguelin@gmail.com>
2023-11-05 11:50:53 +0100

    build: add Tailwind

---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-3xl text-primary font-bold underline">Hello world!</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    colors: {
+      primary: "#1ebea5",
+      secondary: "#273443",
+    },
+  },
+  plugins: [],
+};
