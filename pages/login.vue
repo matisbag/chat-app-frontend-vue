1671711b3a4a6b34a23b13336b9196f984356abe
Matis <matis.baguelin@gmail.com>
2023-12-01 20:20:04 +0100

    style: remove empty style

--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,7 @@
-<template>
-  <AuthForm type="login" />
-</template>
-
 <script lang="ts" setup>
 import AuthForm from "~/components/AuthForm.vue";
 </script>
 
-<style></style>
+<template>
+  <AuthForm type="login" />
+</template>
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,9 +1,7 @@
-<template>
-  <AuthForm type="signup" />
-</template>
-
 <script lang="ts" setup>
 import AuthForm from "~/components/AuthForm.vue";
 </script>
 
-<style></style>
+<template>
+  <AuthForm type="signup" />
+</template>
