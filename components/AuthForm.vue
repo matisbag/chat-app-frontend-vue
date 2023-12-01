12d41ffebfbc99734ae1cabf0765902c43a6f31b
Matis <matis.baguelin@gmail.com>
2023-12-01 01:53:18 +0100

    refactor(AuthForm): use Nuxt UI

--- components/AuthForm.vue
+++ components/AuthForm.vue
@@ -1,93 +1,78 @@
-<template>
-  <main class="flex flex-col items-center justify-center px-6 py-8 lg:py-0">
-    <div
-      class="w-full rounded-lg shadow border md:mt-0 sm:max-w-xl xl:p-0 bg-gray-800 border-gray-700"
-    >
-      <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-        <h1
-          class="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white"
-        >
-          {{ loginPage ? "Login" : "Create account" }}
-        </h1>
-        <form
-          class="space-y-4 md:space-y-6"
-          @submit.prevent="loginPage ? onLogin() : onLogin()"
-        >
-          <div>
-            <label
-              for="email"
-              class="block mb-2 text-sm font-medium text-white"
-            >
-              Email
-            </label>
-            <input
-              v-model="form.email"
-              type="email"
-              id="email"
-              class="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 dark:placeholder-gray-400 text-white focus:ring-primary focus:border-primary"
-              placeholder="example@gmail.com"
-            />
-          </div>
-          <div>
-            <label
-              for="password"
-              class="block mb-2 text-sm font-medium text-white"
-            >
-              Password
-            </label>
-            <input
-              v-model="form.password"
-              type="password"
-              id="password"
-              placeholder="••••••••"
-              class="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 dark:placeholder-gray-400 text-white focus:ring-primary focus:border-primary"
-            />
-          </div>
-          <button
-            type="submit"
-            class="w-full text-white hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-secondary hover:bg-primary focus:ring-primary"
-          >
-            {{ loginPage ? "Log in" : "Create" }}
-          </button>
-          <p class="text-gray-400">
-            Or
-            <NuxtLink
-              :to="loginPage ? '/signup' : '/login'"
-              class="font-medium hover:underline"
-            >
-              {{ loginPage ? "sign up" : "Login" }}
-            </NuxtLink>
-          </p>
-        </form>
-      </div>
-    </div>
-  </main>
-</template>
+<script setup lang="ts">
+import { z } from "zod";
+import type { FormSubmitEvent } from "#ui/types";
 
-<script lang="ts" setup>
-import type { UserForm } from "~/types/user";
 const props = defineProps<{
   type: "login" | "signup";
 }>();
 
 const router = useRouter();
 const { login } = useAuth();
-
-const form = reactive<UserForm>({
-  email: "",
-  password: "",
+const state = reactive({
+  email: undefined,
+  password: undefined,
 });
-
 const loginPage = computed(() => props.type === "login");
 
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(4, "Must be at least 4 characters"),
+});
+
+type Schema = z.output<typeof schema>;
+
 // TODO: add signup function
-// TODO: form validation
-async function onLogin() {
+async function onSubmit(event: FormSubmitEvent<Schema>) {
   try {
-    await login(form.email, form.password);
+    await login(event.data.email, event.data.password);
     router.push("/c");
   } catch (error: any) {
     console.error(error);
   }
 }
 </script>
+
+<template>
+  <main class="flex flex-col items-center justify-center px-6 py-8 lg:py-0">
+    <UCard as="section" class="w-full sm:max-w-xl">
+      <template #header>
+        <h1 class="text-xl font-bold leading-tight tracking-tight md:text-2xl">
+          {{ loginPage ? "Login" : "Create account" }}
+        </h1>
+      </template>
+
+      <UForm
+        :schema="schema"
+        :state="state"
+        class="space-y-4 md:space-y-6"
+        @submit="onSubmit"
+      >
+        <UFormGroup label="Email" name="email">
+          <UInput v-model="state.email" placeholder="example@gmail.com" />
+        </UFormGroup>
+
+        <UFormGroup label="Password" name="password">
+          <UInput
+            v-model="state.password"
+            placeholder="••••••••"
+            type="password"
+          />
+        </UFormGroup>
+
+        <UButton type="submit" size="md" block>
+          {{ loginPage ? "Log in" : "Create" }}
+        </UButton>
+
+        <p class="text-gray-600">
+          Or
+          <NuxtLink
+            :to="loginPage ? '/signup' : '/login'"
+            class="font-medium hover:underline"
+          >
+            {{ loginPage ? "sign up" : "Login" }}
+          </NuxtLink>
+        </p>
+      </UForm>
+    </UCard>
+  </main>
+</template>
