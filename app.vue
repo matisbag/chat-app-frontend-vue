4633b3cfa1c7a1163bd166b0b91d1c5cbe01d229
Matis <matis.baguelin@gmail.com>
2023-12-05 23:56:37 +0100

    feat: adding head title and description

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useHead({
+  title: "Chat app",
+  meta: [
+    { name: "description", content: "Vue.js real-time chat application." },
+  ],
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
