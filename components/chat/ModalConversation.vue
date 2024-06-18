b20ee5418881c7a3ad6047edb94f9be37262d674
Matis <matis.baguelin@gmail.com>
2024-06-18 17:07:01 +0200

    feat: can create conversation

--- components/chat/ModalConversation.vue
+++ components/chat/ModalConversation.vue
@@ -1,44 +1,49 @@
 <script setup lang="ts">
-const props = defineProps<{
-  modelValue: boolean
-}>()
+import type { UserWithoutPassword } from "~/types/user"
 
-const emit = defineEmits(["update:modelValue"])
-
-// Utilisation de computed pour créer un getter et un setter pour isOpen
-const isOpen = computed({
-  get() {
-    return props.modelValue
-  },
-  set(value: boolean) {
-    emit("update:modelValue", value)
-  },
-})
+const model = defineModel<boolean>()
+const { $api } = useNuxtApp()
 
 const state = reactive({
   title: "",
   description: "",
-  userIds: [] as string[],
+  userIds: [],
 })
+const loadingSearch = ref(false)
+const loadingSubmit = ref(false)
 
-function handleSubmit(event: Event) {
-  event.preventDefault()
-  // props.onSubmit(state)
-  isOpen.value = false
+async function search(q: string) {
+  loadingSearch.value = true
+
+  const users = await $api<UserWithoutPassword[]>("/users", {
+    params: { pseudo: q },
+  })
+
+  loadingSearch.value = false
+
+  return users
+}
+
+async function handleSubmit() {
+  loadingSubmit.value = true
+
+  await $api("/conversations", {
+    method: "POST",
+    body: state,
+  })
+
+  model.value = false
+  loadingSubmit.value = false
 }
 </script>
 
 <template>
-  <UModal v-model="isOpen">
+  <UModal v-model="model">
     <UCard class="w-full">
       <template #header>Créer une conversation</template>
 
-      <UForm
-        :state="state"
-        class="space-y-4 md:space-y-6"
-        @submit="handleSubmit"
-      >
-        <UFormGroup label="Title" name="title">
+      <UForm :state="state" class="space-y-4 md:space-y-6">
+        <UFormGroup label="Title " name="title">
           <UInput v-model="state.title" placeholder="Title" required />
         </UFormGroup>
         <UFormGroup label="Description" name="description">
@@ -47,12 +52,22 @@ function handleSubmit(event: Event) {
         <UFormGroup label="Utilisateurs" name="userIds">
           <USelectMenu
             v-model="state.userIds"
-            placeholder="Utilisateurs"
+            :loading="loadingSearch"
+            :searchable="search"
+            placeholder="Search for a user..."
+            value-attribute="id"
+            option-attribute="pseudo"
             multiple
+            trailing
           />
         </UFormGroup>
-        <UButton type="submit" block>Créer</UButton>
       </UForm>
+
+      <template #footer>
+        <UButton :loading="loadingSubmit" block @click="handleSubmit">
+          Créer
+        </UButton>
+      </template>
     </UCard>
   </UModal>
 </template>
